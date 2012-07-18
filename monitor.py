8db6f388069936acecd315783bafebdf6a215902
Tod <todjiang@gmail.com>
2012-07-19 00:27:26 +0800

    add schdule pull log

--- monitor.py
+++ monitor.py
@@ -2,11 +2,14 @@
 import re
 import os
 import logging
+import threading
+import time
 from datetime import datetime
 
+import tornado.database
 from gen_diff import GenDiffer
 
-import tornado.database
+
 
 svn_url_prefix = "http://svn.sc4.paypal.com/svn/projects"
 #svn_url_prefix = "http://v8.googlecode.com/svn"
@@ -142,30 +145,78 @@ def delete_file_if_exists(file):
 	if os.path.isfile(file):
 		os.remove(file)
 		logging.info('delete file: %s', file)
-
 		
 def pull_log(svn_url, log_file_name):
 	delete_file_if_exists(log_file_name)
 	cmd = "svn -v log " + svn_url + " -l 100 >> " + log_file_name
 	ret = os.system(cmd)
 
+def pull(file_name):
+	delete_file_if_exists(file_name)
+	cmd = "svn -v log " + svn_url + " -l 100 >> " + file_name
+	ret = os.system(cmd)
+
+	
+def get_last_changed_version(svn_url, file_name):
+	delete_file_if_exists(file_name)
+	cmd = "svn info " + svn_url + " >> " + file_name
+	ret = os.system(cmd)
+	f = open(file_name)
+	for l in f:
+		if l.find('Last Changed Rev') <> -1:
+			last_rev = l[18:-1]
+			logging.info("Last Changed Rev:" + last_rev) 
+			f.close()
+			return last_rev
+	f.close()
+
+max_version=None
+def get_max_version():
+	global max_version
+	if max_version == None:
+		r = db.get("SELECT max(version) as max FROM svn_log")
+		max_version = r['max']
+	return int(max_version)
 		
+		
+def check_action(svn_url):
+	print "Start check action..."
+	
+	svn_max = get_last_changed_version(svn_url, "temp_svn.log")
+	current_max = get_max_version()
+
+	if int(svn_max) > current_max:
+		log_file_name = "svn_history.log"
+		delete_file_if_exists(log_file_name)
+		cmd = "svn -v log " + svn_url + " -r " + str(current_max+1) + ":" + svn_max + " >> " + log_file_name
+		logging.info("svn pull cmd: " + cmd)
+		ret = os.system(cmd)
+
+		f = open(log_file_name)
+		assemble(f)
+		f.close()
+
 log_file_name = "ts-decision-kernel-1-28_workflow.log" 
 svn_url = "http://svn.sc4.paypal.com/svn/projects/risk/frameworks/IDI/workflow/branches/ts-decision-kernel-1-28"
+
+def schdule():
+	threading.Timer(60, schdule).start()
+	check_action(svn_url)
+
 
 def main():
 	logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='monitor.log', level=logging.DEBUG)
 	logging.info('Started...')
-	
-	logging.info('Pull svn log...')
-	pull_log(svn_url, log_file_name)
+	# invoke pull log at first time
+	#logging.info('Pull svn log...')
+	#pull_log(svn_url, log_file_name)
 
-	file = open(log_file_name)
-
-	assemble(file)
-	file.close()
+	schdule()
 	logging.info('Finished...')
 
 if __name__ == '__main__':
     main()
+	
+
+
 	
--- pull.py
+++ pull.py
@@ -1,12 +1,13 @@
 import os
 import threading
+import logging
 
 def delete_file_if_exists(file):
 	if os.path.isfile(file):
 		os.remove(file)
 		print "delete file: " + file
 				
-svn_url = "http://svn.sc4.paypal.com/svn/projects/risk/frameworks/IDI/analytics/branches/ts-decision-kernel-1-28"
+svn_url = "http://svn.sc4.paypal.com/svn/projects/risk/frameworks/IDI/workflow/branches/ts-decision-kernel-1-28"
 #svn_url = "http://v8.googlecode.com/svn/trunk/src/"
 file_name = "ts-decision-kernel-1-28_svninfo.txt"
 
@@ -23,34 +24,38 @@ def get_last_changed_version(svn_url, file_name):
 	f = open(file_name)
 	for l in f:
 		if l.find('Last Changed Rev') <> -1:
-			print l[18:]
-			return l[18:]
+			last_rev = l[18:-1]
+			logging.info("Last Changed Rev:" + last_rev) 
+			f.close()
+			return last_rev
 	f.close()
-
-
-#get_last_changed_version(svn_url, file_name)
 
 max_version=None
 def get_max_version():
 	global max_version
 	if max_version == None:
 		import tornado.database
-		db = tornado.database.Connection("localhost:3306", "test_pull", "root", "mysql")
+		db = tornado.database.Connection("localhost:3306", "stannis", "root", "mysql")
 		r = db.get("SELECT max(version) as max FROM svn_log")
 		max_version = r['max']
 	return max_version
 		
 		
 def check_action():
+	logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='monitor.log', level=logging.DEBUG)
 	svn_max = get_last_changed_version(svn_url, file_name)
 	current_max = get_max_version()
-	if svn_max > current_max:
-		pass
+
+	if int(svn_max) > int(current_max):
+		log_file_name = "svn_history.log"
+		#delete_file_if_exists(log_file_name)
+		cmd = "svn -v log " + svn_url + " -r " + current_max + ":" + svn_max + " >> " + log_file_name
+		logging.info("svn pull cmd: " + cmd)
+		ret = os.system(cmd)
 
 
 def schdule_pull():
   threading.Timer(10, schdule_pull).start()
-  check_action()
+  #check_action()
 
 
-pull(file_name)
