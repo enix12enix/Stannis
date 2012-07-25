c9b777eb4f714dc487cfd801cea6c87a5bfe130b
Tod <todjiang@gmail.com>
2012-07-25 15:34:02 +0800

    update db schema

--- tables.sql
+++ tables.sql
@@ -1,138 +1,136 @@
-drop table if exists `stannis`.`search_history`;
-drop table if exists `stannis`.`svn_diffs`;
-drop table if exists `stannis`.`svn_change_path`;
-drop table if exists `stannis`.`svn_log`;
-drop table if exists  `stannis`.`svn_linked_change_path`; 
-drop table if exists  `stannis`.`svn_module`; 
-
-CREATE  TABLE `stannis`.`svn_log` (
-
-  `id` INT NOT NULL AUTO_INCREMENT,
-
-  `version` VARCHAR(45) NULL ,
-
-  `acct_name` VARCHAR(45) NULL ,
-
-  `date_time` DATETIME NULL ,
-
-  `comments` TEXT NULL ,
-
-  PRIMARY KEY (`id`) );
-  
-  
-  CREATE  TABLE `stannis`.`svn_change_path` (
-
-  `id` INT NOT NULL AUTO_INCREMENT ,
-
-  `action` CHAR(1) NULL ,
-
-  `path` MEDIUMTEXT  NULL ,
-  
-  `filename` VARCHAR(255) NULL ,
-
-  `f_id` INT NULL ,
-
-  PRIMARY KEY (`id`) ,
-
-  INDEX `fid` (`f_id` ASC) ,
-
-  CONSTRAINT `fid`
-
-    FOREIGN KEY (`f_id` )
-
-    REFERENCES `stannis`.`svn_log` (`id` )
-
-    ON DELETE CASCADE
-
-    ON UPDATE NO ACTION);
-
-
-CREATE  TABLE `stannis`.`search_history` (
-
-  `id` INT NOT NULL AUTO_INCREMENT ,
-
-  `ip` VARCHAR(15) NULL COMMENT 'only ip v4' ,
-
-  `content` VARCHAR(60) NULL COMMENT 'search content(currently only user NT name)' ,
-
-  PRIMARY KEY (`id`) ,
-
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
-
-COMMENT = 'user search history';
-
-
-CREATE  TABLE `stannis`.`svn_diffs` (
-
-  `id` INT NOT NULL AUTO_INCREMENT ,
-
-  `f_cp_id` INT NULL ,
-
-  `diff` MEDIUMTEXT NULL ,
-
-  PRIMARY KEY (`id`) ,
-
-  INDEX `fkfk_cp_id` (`f_cp_id` ASC) ,
-
-  CONSTRAINT `f_cp_id`
-
-    FOREIGN KEY (`f_cp_id` )
-
-    REFERENCES `stannis`.`svn_change_path` (`id` )
-
-    ON DELETE CASCADE
-
-    ON UPDATE NO ACTION)
-
-COMMENT = 'tables store code diff html';
-
-CREATE  TABLE `stannis`.`svn_module` (
-
-  `id` INT NOT NULL AUTO_INCREMENT ,
-
-  `name` VARCHAR(60) NULL ,
-
-  `path` MEDIUMTEXT NULL ,
-
-  `r_id` INT NULL ,
-
-  PRIMARY KEY (`id`) )
-
-COMMENT = 'modules(branches)/submodules releationship';
-
-ALTER TABLE `stannis`.`svn_log` ADD COLUMN `m_id` INT NULL  AFTER `comments` , 
-
-  ADD CONSTRAINT `m_id`
-
-  FOREIGN KEY (`m_id` )
-
-  REFERENCES `stannis`.`svn_module` (`id` )
-
-  ON DELETE NO ACTION
-
-  ON UPDATE NO ACTION
-
-, ADD INDEX `m_id` (`m_id` ASC) ;
-
-CREATE  TABLE `stannis`.`svn_linked_change_path` (
-
-  `id` INT NOT NULL AUTO_INCREMENT ,
-
-  `path` MEDIUMTEXT NULL ,
-
-  PRIMARY KEY (`id`) );
-
-  
-ALTER TABLE `stannis`.`svn_change_path` ADD COLUMN `f_link_cp_id` INT NULL  AFTER `f_id` , 
-
-  ADD CONSTRAINT `f_lcp_id`
-
-  FOREIGN KEY (`f_link_cp_id` )
-
-  REFERENCES `stannis`.`svn_linked_change_path` (`id` )
-
-  ON DELETE NO ACTION
-
-  ON UPDATE NO ACTION
-
-, ADD INDEX `f_lcp_id` (`f_link_cp_id` ASC) ;
+CREATE DATABASE  IF NOT EXISTS `stannis` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `stannis`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: stannis
+-- ------------------------------------------------------
+-- Server version 5.5.9
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `svn_linked_change_path`
+--
+
+DROP TABLE IF EXISTS `svn_linked_change_path`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `svn_linked_change_path` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` mediumtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `svn_module`
+--
+
+DROP TABLE IF EXISTS `svn_module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `svn_module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) DEFAULT NULL,
+  `path` mediumtext,
+  `r_id` int(11) DEFAULT NULL,
+  `level` int(3) DEFAULT NULL,
+  `active` int(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='modules(branches)/submodules releationship';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `svn_diffs`
+--
+
+DROP TABLE IF EXISTS `svn_diffs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `svn_diffs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `f_cp_id` int(11) DEFAULT NULL,
+  `diff` mediumtext,
+  PRIMARY KEY (`id`),
+  KEY `fkfk_cp_id` (`f_cp_id`),
+  CONSTRAINT `f_cp_id` FOREIGN KEY (`f_cp_id`) REFERENCES `svn_change_path` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1809 DEFAULT CHARSET=utf8 COMMENT='tables store code diff html';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `search_history`
+--
+
+DROP TABLE IF EXISTS `search_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `search_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(15) DEFAULT NULL COMMENT 'only ip v4',
+  `content` varchar(60) DEFAULT NULL COMMENT 'search content(currently only user NT name)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COMMENT='user search history';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `svn_change_path`
+--
+
+DROP TABLE IF EXISTS `svn_change_path`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `svn_change_path` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action` char(1) DEFAULT NULL,
+  `path` mediumtext,
+  `filename` varchar(255) DEFAULT NULL,
+  `f_id` int(11) DEFAULT NULL,
+  `f_link_cp_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fid` (`f_id`),
+  KEY `f_lcp_id` (`f_link_cp_id`),
+  CONSTRAINT `f_lcp_id` FOREIGN KEY (`f_link_cp_id`) REFERENCES `svn_linked_change_path` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fid` FOREIGN KEY (`f_id`) REFERENCES `svn_log` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2641 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `svn_log`
+--
+
+DROP TABLE IF EXISTS `svn_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `svn_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` varchar(45) DEFAULT NULL,
+  `acct_name` varchar(45) DEFAULT NULL,
+  `date_time` datetime DEFAULT NULL,
+  `comments` text,
+  `m_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `m_id` (`m_id`),
+  CONSTRAINT `m_id` FOREIGN KEY (`m_id`) REFERENCES `svn_module` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-07-24  9:28:54
